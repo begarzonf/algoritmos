68921cc447e0854525636017f55334445a5c4348
begarzonf <begarzonf@unal.edu.co>
2018-02-25 13:05:14 -0500

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,143 @@
+// variable int 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+		int n;
+		cin >> n;
+    int a=0;
+    int b=1;
+    int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+      }
+        cout << endl << c << endl; 
+    }
+ 
+	return 0;
+}
+
+// unsigned int overflow con n>4300000000
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+		unsigned int n;
+		cin >> n;
+    unsigned int a=0;
+    unsigned int b=1;
+    unsigned int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(unsigned int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+       }
+       cout << endl << c << endl; 
+     }
+ 
+	return 0;
+}
+
+// short int
+#include <iostream>
+using namespace std;
+
+int main() {
+		short int n;
+		cin >> n;
+    short int a=0;
+    short int b=1;
+    short int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(short int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+      }
+        cout << endl << c << endl; 
+    }
+ 
+	return 0;
+}
+// unsigned short int
+#include <iostream>
+using namespace std;}
+
+int main() {
+		unsigned short int n;
+		cin >> n;
+    unsigned short int a=0;
+    unsigned short int b=1;
+    unsigned short int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(unsigned short int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+      }
+        cout << endl << c << endl; 
+    }
+ 
+	return 0;
+}
+// long int
+#include <iostream>
+using namespace std;
+
+int main() {
+		long int n;
+		cin >> n;
+    long int a=0;
+    long int b=1;
+    long int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(long int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+      }
+        cout << endl << c << endl; 
+    }
+ 
+	return 0;
+}
+
+// unsigned long int
+#include <iostream>
+using namespace std;
+
+int main() {
+		unsigned long int n;
+		cin >> n;
+    unsigned long int a=0;
+    unsigned long int b=1;
+    unsigned long int c=0;
+    if (n==1 || n==2){
+      cout << endl << 1 << endl; 
+    }else{
+      for(unsigned long int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+      }
+        cout << endl << c << endl; 
+    }
+ 
+	return 0;
+}
